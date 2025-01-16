d3cde9eadf874a1a79701603441d89ee94bdc47a
JoseGoncalvess <joseinacio774@gmail.com>
2025-01-16 09:55:44 -0300

    [Ajust] Update porjetct

--- cardvalidation/lib/main.dart
+++ cardvalidation/lib/main.dart
@@ -1,15 +1,8 @@
-import 'package:cardvalidation/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'main_app.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false, home: Home());
-  }
-}
